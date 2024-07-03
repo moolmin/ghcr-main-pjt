c79cf9045abdc2da70c99cd2d3be0e500609a873
moolmin <zzmzzm0065@gmail.com>
2024-07-03 15:44:43 +0900

    initial commit

---
+++ src/test/java/com/example/ghcrmainpjt/GhcrMainPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ghcrmainpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhcrMainPjtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
